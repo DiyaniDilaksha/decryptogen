3e6ff64061b53aa62879a296aab7d4fa7017d035
Diyani Munasinghe <DIYANIM@mobitel.lk>
2023-03-14 11:31:11 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+import { ServicesComponent } from './services/services.component';
+
+const routes: Routes = [
+  { path: '', component:HomeComponent},
+  { path: 'AboutUs', component:AboutUsComponent },
+  { path: 'Services', component:ServicesComponent },
+  { path: 'ContactUs', component:ContactUsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [
+  AboutUsComponent,ServicesComponent,ContactUsComponent
+]
