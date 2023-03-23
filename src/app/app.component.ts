ce65385220c5c9287595415b12de7877ebd95d7f
Diyani Munasinghe <DIYANIM@mobitel.lk>
2023-03-23 11:13:31 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'website';
+  constructor(){
+
+  }
+  menuVariable:boolean = false;
+  openMenu(){
+    this.menuVariable =! this.menuVariable;
+  }
 }
